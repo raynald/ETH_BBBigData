0a844c6c31b3d80db9958b7dbb614ef1c74ef7a8
skyshaker <ageekinside@gmail.com>
2014-10-24 22:46:48 +0200

    stations to features
    bug fixes

--- stations_to_features.py
+++ stations_to_features.py
@@ -8,20 +8,22 @@ if __name__ == "__main__":
 			continue
 		path = './stations/' + station_id
 		f = open(path, "r")
-		features = [dict() for i in range(1,13)]
+		features = [dict() for i in range(20)]
 		print station_id
 		flag = True
 		curmonth = 0
 		month_counts = {'PRCP':0, 'SNOW':0, 'SNWD':0, 'TMAX':0, 'TMIN':0}
 		month_sums = {'PRCP':0, 'SNOW':0, 'SNWD':0, 'TMAX':0, 'TMIN':0}
+		required_codes = {'PRCP', 'SNOW', 'SNWD', 'TMAX', 'TMIN'}
 		for line in f:		
 			line = line.strip()
-
 			year = int(line[0:4])
 			month = int(line[4:6])
 			day = int(line[6:8])
 			code = line[9:13]
 			num = int(line[14:])
+			if code not in required_codes:
+				continue
 			if flag:
 				curmonth = month
 				flag = False
@@ -41,9 +43,10 @@ if __name__ == "__main__":
 				month_sums[code] = month_sums[code] + num
 		for k in month_counts.keys():
 			if month_counts[k] == 0:
-					features[curmonth-1][k] = 'Missing'
+					features[curmonth][k] = 'Missing'
 			else:
-					features[curmonth-1][k] = month_sums[k] / month_counts[k]	
-		for i in range(1,month+1):
+					features[curmonth][k] = month_sums[k] / month_counts[k]
+		f_out=open("./features/" + station_id, "a")					
+		for i in range(1,curmonth+1):
 			for j in features[i].keys():
-				print i ,j, features[i][j]
+				f_out.write(str(i)+" "+j+" "+str(features[i][j])+"\n")
