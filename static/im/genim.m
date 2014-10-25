69ea4e5985ce7f5f4f7865b28a3796228ad9e3e4
JianGoForIt <zhangjianthu@gmail.com>
2014-10-25 23:18:06 +0200

    random image

---
+++ static/im/genim.m
@@ -0,0 +1,14 @@
+num_im = 300;
+value = zeros(num_im, 3);
+for k = 1:3
+    value(:, k) = linspace(0, 255, num_im);
+    value(:, k) = value(randperm(length(value) ), k);
+end
+edge_size = 10;
+im = zeros(edge_size, edge_size, 3);
+for k = 1:num_im
+    for l = 1:3
+        im(:,:, l) = ones(edge_size, edge_size) * value(k, l);
+    end
+    imwrite(uint8(im), ['.\', num2str(k), '.jpg'], 'jpg');
+end
