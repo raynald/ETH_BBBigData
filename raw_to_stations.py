7319423b7706cac23859f9de31cc9fedf8586c6f
raynald <raynaldchung@gmail.com>
2014-11-22 14:50:28 +0100

    rewrite mapreduce

--- raw_to_stations.py
+++ raw_to_stations.py
@@ -1,14 +1,15 @@
 import sys
-#
-#Sample input: US1FLSL0019,20090101,PRCP,0,,,N,
-#
+"""
+Sample input: US1FLSL0019,20090101,PRCP,0,,,N,
+Sample output: SP000008027 20090101    TMAX    146
+"""
 
 if __name__ == "__main__":
     for line in sys.stdin:
         line = line.strip()
         station_id = line[0:11]
-        year = line[12:20]
+        date = line[12:20]
         code = line[21:25]
         num = line[26:].split(',')[0]
-        print "%s\t%s\t%s\t%s" % (station_id, year, code, num)
+        print "%s\t%s\t%s\t%s" % (station_id, date, code, num)
 
--- stations_to_features.py
+++ stations_to_features.py
@@ -1,90 +1,27 @@
 import sys
-import os
-import numpy
+import numpy as np
+
+codeSet = {'PRCP':0, 'SNOW':1, 'SNWD':2, 'TMAX':3, 'TMIN':4}
+
+feature = np.array([])
+old_key = ""
+old_year = ""
+old_month = ""
 
 if __name__ == "__main__":
-	filelist = os.listdir('./stations')
-	features_means_values = [dict({'PRCP':[], 'SNOW':[], 'SNWD':[], 'TMAX':[], 'TMIN':[]}) for i in range(15)]
-	features_variances_values = [dict({'PRCP':[], 'SNOW':[], 'SNWD':[], 'TMAX':[], 'TMIN':[]}) for i in range(15)]
-	features_means = [dict({'PRCP':0, 'SNOW':0, 'SNWD':0, 'TMAX':0, 'TMIN':0}) for i in range(15)]
-	features_variances = [dict({'PRCP':0, 'SNOW':0, 'SNWD':0, 'TMAX':0, 'TMIN':0}) for i in range(15)]
-	count = 0
-	for station_id in filelist: # calculate features
-		if station_id[0] == '.': #system files
-			continue
-		count += 1
-		if count % 1000 == 0:
-			print count
-		path = './stations/' + station_id
-		f = open(path, "r")
-		file_features_counts= [dict({'PRCP':0, 'SNOW':0, 'SNWD':0, 'TMAX':0, 'TMIN':0}) for i in range(15)]
-		file_features_values = [dict({'PRCP':[], 'SNOW':[], 'SNWD':[], 'TMAX':[], 'TMIN':[]}) for i in range(15)]
-		file_features_means = [dict({'PRCP':0, 'SNOW':0, 'SNWD':0, 'TMAX':0, 'TMIN':0}) for i in range(15)]
-		file_features_variances = [dict({'PRCP':0, 'SNOW':0, 'SNWD':0, 'TMAX':0, 'TMIN':0}) for i in range(15)]
-		required_codes = {'PRCP', 'SNOW', 'SNWD', 'TMAX', 'TMIN'}
-		for line in f:		
-			line = line.strip()
-			year = int(line[0:4])
-			month = int(line[4:6])
-			day = int(line[6:8])
-			code = line[9:13]
-			num = int(line[14:])
-			if code not in required_codes:
-				continue
-			file_features_values[month][code].append(num)
-			file_features_counts[month][code] = file_features_counts[month][code]  + 1
-		for i in range(1,13):
-			for j in required_codes:
-				if file_features_counts[i][j] > 0:
-					file_features_means[i][j] = numpy.mean(file_features_values[i][j])
-					features_means_values[i][j].append(file_features_means[i][j])
-					file_features_variances[i][j] = numpy.var(file_features_values[i][j])
-					#file_features_variances[i][j] = numpy.std(file_features_values[i][j]) #std
-					features_variances_values[i][j].append(file_features_variances[i][j])
-				else:
-					file_features_means[i][j] = "Missing"
-					file_features_variances[i][j] = "Missing"
-	for i in range(1,13):
-			for j in required_codes:
-				features_means[i][j] = numpy.mean(features_means_values[i][j])
-				features_variances[i][j] = numpy.mean(features_variances_values[i][j])
-	count = 0
-	for station_id in filelist: # calculate features
-		if station_id[0] == '.': #system files
-			continue
-		count += 1
-		if count % 1000 == 0:
-			print count
-		path = './stations/' + station_id
-		f = open(path, "r")
-		file_features_counts= [dict({'PRCP':0, 'SNOW':0, 'SNWD':0, 'TMAX':0, 'TMIN':0}) for i in range(15)]
-		file_features_values = [dict({'PRCP':[], 'SNOW':[], 'SNWD':[], 'TMAX':[], 'TMIN':[]}) for i in range(15)]
-		file_features_means = [dict({'PRCP':0, 'SNOW':0, 'SNWD':0, 'TMAX':0, 'TMIN':0}) for i in range(15)]
-		file_features_variances = [dict({'PRCP':0, 'SNOW':0, 'SNWD':0, 'TMAX':0, 'TMIN':0}) for i in range(15)]
-		required_codes = {'PRCP', 'SNOW', 'SNWD', 'TMAX', 'TMIN'}
-		for line in f:		
-			line = line.strip()
-			year = int(line[0:4])
-			month = int(line[4:6])
-			day = int(line[6:8])
-			code = line[9:13]
-			num = int(line[14:])
-			if code not in required_codes:
-				continue
-			file_features_values[month][code].append(num)
-			file_features_counts[month][code] = file_features_counts[month][code]  + 1
-		for i in range(1,13):
-			for j in required_codes:
-				if file_features_counts[i][j] > 0:
-					file_features_means[i][j] = numpy.mean(file_features_values[i][j])
-					file_features_variances[i][j] = numpy.var(file_features_values[i][j])
-					#file_features_variances[i][j] = numpy.std(file_features_values[i][j]) #std
+    for line in sys.stdin:
+        line = line.strip()
+        key, date, code, num = line.split('\t')
+        year = date[0:4]
+        month = date[4:6]
+        day = date[6:8]
+        if code in codeSet:
+            if key == old_key and year == old_year and month == old_month:
+                feature = np.append(feature, [int(num)])
+            else:
+                print "%s\t%s\t%s\t%f\t%f" % (old_key, old_year, old_month, feature.mean(), feature.var())
+                old_key = key
+                old_year = year
+                old_month = month
+                feature = np.array([int(num)])
 
-				else:
-					file_features_means[i][j] = features_means[i][j]
-					file_features_variances[i][j] = features_variances[i][j]
-		f_out = open("./features/" + station_id+".csv", "a")
-		for i in range(1,13):
-			for j in required_codes:
-				f_out.write(str(file_features_means[i][j])+","+str(file_features_variances[i][j])+",")
-	print "done"
