c9c9cc06b81e07e4e61d4234e45d2530081ea8d9
raynald <raynaldchung@gmail.com>
2014-11-22 19:43:17 +0100

    update

--- stations_to_features.py
+++ stations_to_features.py
@@ -3,7 +3,12 @@ import sys
 import numpy as np
 
 codeSet = {'PRCP':0, 'SNOW':1, 'SNWD':2, 'TMAX':3, 'TMIN':4}
-feature = np.array([])
+feature = []
+feature += [np.array([])]
+feature += [np.array([])]
+feature += [np.array([])]
+feature += [np.array([])]
+feature += [np.array([])]
 old_key = ""
 old_year = ""
 old_month = ""
@@ -14,12 +19,16 @@ if __name__ == "__main__":
         key, year, month, code, num = line.split('\t')
         if code in codeSet:
             if key == old_key and year == old_year and month == old_month:
-                feature = np.append(feature, [int(num)])
+                feature[codeSet[code]] = np.append(feature[codeSet[code]], [int(num)])
             else:
-                print "%s\t%s\t%s\t%f\t%f" % (old_key, old_year, old_month, feature.mean(), feature.var())
+                for i in range(5):
+                    if feature[i].size > 0:
+                        print "%s\t%s\t%s\t%f\t%f" % (old_key, old_year, old_month, feature[i].mean(), feature[i].var())
+                        feature[i] = np.array([])
                 old_key = key
                 old_year = year
                 old_month = month
-                feature = np.array([int(num)])
-                if feature.size > 0:
-                    print "%s\t%s\t%s\t%f\t%f" % (old_key, old_year, old_month, feature.mean(), feature.var())
+                feature[codeSet[code]] = np.append(feature[codeSet[code]], [int(num)])
+    for i in range(5):
+        if feature[i].size > 0:
+            print "%s\t%s\t%s\t%f\t%f" % (old_key, old_year, old_month, feature[i].mean(), feature[i].var())
