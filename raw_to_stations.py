4276d167ceebcd1b7cc5cd18b6a916ddec538fe4
raynald <raynaldchung@gmail.com>
2014-11-22 13:49:14 +0100

    raw_to_stations update

--- raw_to_stations.py
+++ raw_to_stations.py
@@ -3,8 +3,6 @@ import sys
 #Sample input: US1FLSL0019,20090101,PRCP,0,,,N,
 #
 
-#def :
-
 if __name__ == "__main__":
     for line in sys.stdin:
         line = line.strip()
@@ -12,6 +10,5 @@ if __name__ == "__main__":
         year = line[12:20]
         code = line[21:25]
         num = line[26:].split(',')[0]
-        with open("./stations/" + station_id, "a") as myfile:
-                myfile.write(year + " " + code+ " " + num + "\n")
+        print "%s\t%s\t%s\t%s" % (station_id, year, code, num)
 
